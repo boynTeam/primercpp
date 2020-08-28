46a103f0aa029a48b8e90f5edeb7b719d233f958
BoynChan <chankaser@gmail.com>
2020-08-28 23:11:43 +0800

    format code

--- char12/TextQuery.h
+++ char12/TextQuery.h
@@ -8,32 +8,33 @@
 #include <map>
 #include <sstream>
 
+class QueryResult
+{
+    friend std::ostream &print(std::ostream &, const QueryResult &);
 
-
-class QueryResult {
-    friend std::ostream& print(std::ostream&,const QueryResult&);
-    public:
+public:
     QueryResult(std::string s,
-        std::shared_ptr<std::vector<std::string>> f,
-        std::shared_ptr<std::set<int>> p):
-        word(s),lines(p),file(f){}
-    private:
+                std::shared_ptr<std::vector<std::string>> f,
+                std::shared_ptr<std::set<int>> p) : word(s), lines(p), file(f) {}
+
+private:
     std::string word;
     std::shared_ptr<std::vector<std::string>> file;
     std::shared_ptr<std::set<int>> lines;
 };
 
-class TextQuery {
-    public:
-    TextQuery(std::ifstream&);
-    QueryResult query(const std::string&);
-    private:
+class TextQuery
+{
+public:
+    TextQuery(std::ifstream &);
+    QueryResult query(const std::string &);
+
+private:
     std::shared_ptr<std::vector<std::string>> file_content;
-    std::map<std::string,std::shared_ptr<std::set<int>>> word_line_map;
+    std::map<std::string, std::shared_ptr<std::set<int>>> word_line_map;
 };
 
-
-std::string toLower(std::string& s);
-std::string removePunct(const std::string& s);
-std::string make_plural(size_t sz,const std::string& word,const std::string& ending);
-std::ostream& print(std::ostream&,const QueryResult&);
+std::string toLower(std::string &s);
+std::string removePunct(const std::string &s);
+std::string make_plural(size_t sz, const std::string &word, const std::string &ending);
+std::ostream &print(std::ostream &, const QueryResult &);
--- char12/query.cc
+++ char12/query.cc
@@ -1,16 +1,18 @@
 #include "TextQuery.h"
 
-
-void runQueries(std::ifstream& f) {
+void runQueries(std::ifstream &f)
+{
     TextQuery tq(f);
-    while(true) {
-        std::cout<<"Enter a word to lookup, or q for quiting." << std:: endl;
+    while (true)
+    {
+        std::cout << "Enter a word to lookup, or q for quiting." << std::endl;
         std::string word;
         std::cin >> word;
-        if (!std::cin||word=="q") {
+        if (!std::cin || word == "q")
+        {
             break;
         }
-        print(std::cout,tq.query(toLower(word)));
+        print(std::cout, tq.query(toLower(word)));
     }
 }
 
